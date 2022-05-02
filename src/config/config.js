4c4540c67d27be980ff27177df97278f1c69f0cd
Slan <snchong.slan@gmail.com>
2022-05-02 17:02:35 +0800

    config update & modular

---
+++ src/config/config.js
@@ -0,0 +1,98 @@
+const SKILLS = [
+  {
+    type: "PL",
+    name: "HTML5",
+    src: "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg",
+  },
+  {
+    type: "PL",
+    name: "CSS",
+    src: "https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg",
+  },
+  {
+    type: "PL",
+    name: "JavaScript",
+    src: "https://upload.vectorlogo.zone/logos/javascript/images/239ec8a4-163e-4792-83b6-3f6d96911757.svg",
+  },
+  {
+    type: "PL",
+    name: "Python",
+    src: "https://www.vectorlogo.zone/logos/python/python-icon.svg",
+  },
+  {
+    type: "PL",
+    name: "C",
+    src: "https://seeklogo.com/images/C/c-logo-672525892C-seeklogo.com.png",
+  },
+  {
+    type: "Frameworks",
+    name: "NodeJS",
+    src: "https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg",
+  },
+  {
+    type: "Frameworks",
+    name: "React",
+    src: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+  },
+  {
+    type: "Frameworks",
+    name: "Flask",
+    src: "https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg",
+  },
+  {
+    type: "Frameworks",
+    name: "Tensorflow",
+    src: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
+  },
+  {
+    type: "Frameworks",
+    name: ">Keras",
+    src: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg",
+  },
+  {
+    type: "Other",
+    name: "SQLite",
+    src: "https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg",
+  },
+  {
+    type: "Other",
+    name: "MySQL",
+    src: "https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg",
+  },
+  {
+    type: "Other",
+    name: "Git",
+    src: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+  },
+  {
+    type: "Other",
+    name: "MATLAB",
+    src: "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png",
+  },
+  {
+    type: "Other",
+    name: "LaTeX",
+    src: "https://upload.wikimedia.org/wikipedia/commons/9/92/LaTeX_logo.svg",
+  },
+  {
+    type: "Software",
+    name: "Blender",
+    src: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Blender_logo_no_text.svg",
+  },
+  {
+    type: "Software",
+    name: "SketchUP",
+    src: "https://raw.githubusercontent.com/file-icons/icons/e6e6e6ac8cb1d91867167c228c00a667f4d47101/svg/SketchUp-Make.svg",
+  },
+  {
+    type: "Software",
+    name: "VS Code",
+    src: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg",
+  },
+  {
+    type: "Software",
+    name: "Spyder",
+    src: "https://upload.wikimedia.org/wikipedia/commons/archive/7/7e/20211122181859%21Spyder_logo.svg",
+  },
+];
+export { SKILLS };
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,42 +1,56 @@
+import { SKILLS } from "../config/config";
+import { v4 as uuidv4 } from "uuid";
 export const Skills = () => {
-    return (
-        <div className="Skills">
-            <h2 className="title">Programming languages</h2>
-            <div className="skillsContainer">
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg" alt="" /><p>HTML5</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg" alt="" /><p>CSS</p></div>
-                <div className="skills"><img src="https://upload.vectorlogo.zone/logos/javascript/images/239ec8a4-163e-4792-83b6-3f6d96911757.svg" alt="" /><p>JavaScript</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/python/python-icon.svg" alt="" /><p>Python</p></div>
-                <div className="skills"><img src="https://seeklogo.com/images/C/c-logo-672525892C-seeklogo.com.png" alt="" /><p>C</p></div>
-            </div>
-            <h2 className="title">Libraries/Frameworks</h2>
-            <div className="skillsContainer">
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg" alt="" /><p>NodeJS</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg" alt="" /><p>React</p></div>
-                <div className="skills"><img src="https://v4.mui.com/static/logo_raw.svg" alt="" /><p>Material-UI</p></div>
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Three.js_Icon.svg" alt="" /><p>Three.js</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="" /><p>Flask</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg" alt="" /><p>OpenCV</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg" alt="" /><p>Tensorflow</p></div>
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg" alt="" /><p>Keras</p></div>
-            </div>
-            <h2 className="title">Others</h2>
-            <div className="skillsContainer">
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" alt="" /><p>SQLite</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg" alt="" /><p>MySQL</p></div>
-                <div className="skills"><img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="" /><p>Git</p></div>
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png" alt="" /><p>MATLAB</p></div>
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/9/92/LaTeX_logo.svg" alt="" /><p>LaTeX</p></div>
-            </div>
-            <h2 className="title">Software</h2>
-            <div className="skillsContainer">
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Blender_logo_no_text.svg" alt="" /><p>Blender</p></div>
-                <div className="skills"><img src="https://raw.githubusercontent.com/file-icons/icons/e6e6e6ac8cb1d91867167c228c00a667f4d47101/svg/SketchUp-Make.svg" alt="" /><p>SketchUP</p></div>
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" alt="" /><p>VS Code</p></div>
-                <div className="skills"><img src="https://upload.wikimedia.org/wikipedia/commons/archive/7/7e/20211122181859%21Spyder_logo.svg" alt="" /><p>Spyder</p></div>
-                <div className="skills"><img src="https://raw.githubusercontent.com/rdimascio/icons/932c4cf6c9e2031abeca1c164baa0f76785c16fe/icons/color/photoshop.svg" alt="" /><p>Photoshop</p></div>
-            </div>
-        </div>
-    )
-}
-
+  return (
+    <div className="Skills">
+      <h2 className="title">Programming languages</h2>
+      <div className="skillsContainer">
+        {SKILLS.map(
+          (skill) =>
+            skill.type === "PL" && (
+              <div className="skills" key={uuidv4()}>
+                <img src={skill.src} alt="" />
+                <p>{skill.name}</p>
+              </div>
+            )
+        )}
+      </div>
+      <h2 className="title">Libraries/Frameworks</h2>
+      <div className="skillsContainer">
+        {SKILLS.map(
+          (skill) =>
+            skill.type === "Frameworks" && (
+              <div className="skills" key={uuidv4()}>
+                <img src={skill.src} alt="" />
+                <p>{skill.name}</p>
+              </div>
+            )
+        )}
+      </div>
+      <h2 className="title">Others</h2>
+      <div className="skillsContainer">
+        {SKILLS.map(
+          (skill) =>
+            skill.type === "Other" && (
+              <div className="skills" key={uuidv4()}>
+                <img src={skill.src} alt="" />
+                <p>{skill.name}</p>
+              </div>
+            )
+        )}
+      </div>
+      <h2 className="title">Software</h2>
+      <div className="skillsContainer">
+        {SKILLS.map(
+          (skill) =>
+            skill.type === "Software" && (
+              <div className="skills" key={uuidv4()}>
+                <img src={skill.src} alt="" />
+                <p>{skill.name}</p>
+              </div>
+            )
+        )}
+      </div>
+    </div>
+  );
+};
