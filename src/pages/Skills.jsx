73d54f456c1ccab268f1e2d447329f5b7554b13b
Slan <snchong.slan@gmail.com>
2022-05-02 17:09:06 +0800

    code simplifier

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,54 +1,34 @@
 import { SKILLS } from "../config/config";
 import { v4 as uuidv4 } from "uuid";
 export const Skills = () => {
+  const contentHandler = (skill) => {
+    return (
+      <div className="skills" key={uuidv4()}>
+        <img src={skill.src} alt="" />
+        <p>{skill.name}</p>
+      </div>
+    );
+  };
   return (
     <div className="Skills">
       <h2 className="title">Programming languages</h2>
       <div className="skillsContainer">
-        {SKILLS.map(
-          (skill) =>
-            skill.type === "PL" && (
-              <div className="skills" key={uuidv4()}>
-                <img src={skill.src} alt="" />
-                <p>{skill.name}</p>
-              </div>
-            )
-        )}
+        {SKILLS.map((skill) => skill.type === "PL" && contentHandler(skill))}
       </div>
       <h2 className="title">Libraries/Frameworks</h2>
       <div className="skillsContainer">
         {SKILLS.map(
-          (skill) =>
-            skill.type === "Frameworks" && (
-              <div className="skills" key={uuidv4()}>
-                <img src={skill.src} alt="" />
-                <p>{skill.name}</p>
-              </div>
-            )
+          (skill) => skill.type === "Frameworks" && contentHandler(skill)
         )}
       </div>
       <h2 className="title">Others</h2>
       <div className="skillsContainer">
-        {SKILLS.map(
-          (skill) =>
-            skill.type === "Other" && (
-              <div className="skills" key={uuidv4()}>
-                <img src={skill.src} alt="" />
-                <p>{skill.name}</p>
-              </div>
-            )
-        )}
+        {SKILLS.map((skill) => skill.type === "Other" && contentHandler(skill))}
       </div>
       <h2 className="title">Software</h2>
       <div className="skillsContainer">
         {SKILLS.map(
-          (skill) =>
-            skill.type === "Software" && (
-              <div className="skills" key={uuidv4()}>
-                <img src={skill.src} alt="" />
-                <p>{skill.name}</p>
-              </div>
-            )
+          (skill) => skill.type === "Software" && contentHandler(skill)
         )}
       </div>
     </div>
