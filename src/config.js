86be2e92e518a4cb636addbaa841fbfcea16a624
Slan <snchong.slan@gmail.com>
2022-06-02 03:28:52 +0800

    filter feature

--- src/config.js
+++ src/config.js
@@ -2,63 +2,71 @@ export const projects = [
   {
     title: "Personal Website",
     img: "/img/avatar.png",
-    discription: "My first personal Website with ReactJS",
-    categorise: ["React", "Open source"],
-    siteUrl: "",
+    discription: "My first personal Website, Built using React ",
+    categorise: ["React"],
     sourceCodeUrl: "https://github.com/slan-chong/snchong-portfolio-main",
+  },
+  {
+    title: "Github profile",
+    img: "/img/Project_IMG04.png",
+    discription: "Show github user repository and the last push commit time",
+    categorise: ["React", "Tailwind"],
+    demoUrl: "https://gh-api-profile.netlify.app/",
+    sourceCodeUrl: "https://github.com/slan-chong/github-profile",
+  },
+  {
+    title: "MERN-stack todolist",
+    img: "/img/Project_IMG03.png",
+    discription:
+      "todolist with React, connecting to MongoDB with ExpressJS. Every time the state is change, it will send a request to the database",
+    categorise: ["React", "MongoDB", "Tailwind", "ExpressJS", "NodeJS"],
+    demoUrl: "https://mern-stack-todolist.netlify.app/",
+    sourceCodeUrl: "https://github.com/slan-chong/mern-todolist",
+  },
+
+  {
+    title: "Research project",
+    img: "/img/Project_IMG01.webp",
+    discription:
+      "Analyze an architectural floor plan database.A Real-Estate Recommender System from Architectural Floor Plan Images",
+    categorise: ["Computer vision", "MATLAB"],
   },
   {
     title: "Final year projects",
     img: "/img/Project_IMG02.webp",
     discription: "Face recognition System for Healthcare Services",
-    categorise: ["Python", "DBMS", "Machine learning", "Open source"],
+    categorise: ["Python", "SQLite", "Tensorflow", "Keras"],
     sourceCodeUrl: "https://github.com/slan-chong/Face-recognition-FYP",
-  },
-  {
-    title: "MERN-stack todolist",
-    img: "/img/Project_IMG03.png",
-    discription: "https://reurl.cc/NAY6Ok",
-    categorise: ["React", "Mongo", "Tailwind"],
-    sourceCodeUrl: "https://github.com/slan-chong/mern-todolist",
-  },
-  {
-    title: "Github profile",
-    img: "/img/Project_IMG04.png",
-    discription: "https://gh-api-profile.netlify.app/",
-    categorise: ["React", "Tailwind"],
-    sourceCodeUrl: "https://github.com/slan-chong/github-profile",
-  },
-  {
-    title: "Research project",
-    img: "/img/Project_IMG01.webp",
-    discription: "Analyze an architectural floor plan database",
-    categorise: ["Computer vision", "Machine learning"],
   },
 ];
 
-export const checkBoxFilters = [
+export const filters = [
   {
     id: 1,
     name: "React",
   },
   {
     id: 2,
-    name: "Open source",
+    name: "MongoDB",
   },
   {
     id: 3,
-    name: "Python",
+    name: "ExpressJS",
   },
   {
     id: 4,
-    name: "DBMS",
+    name: "NodeJS",
   },
   {
     id: 5,
-    name: "Machine learning",
+    name: "Tailwind",
   },
   {
     id: 6,
     name: "Computer vision",
+  },
+  {
+    id: 7,
+    name: "Python",
   },
 ];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,106 @@
-import { useEffect } from "react";
-import { ProjectCard } from "../components/ProjectCard";
+import { useEffect, useState } from "react";
+import { projects, filters } from "../config";
+import { BsGithub } from "react-icons/bs";
 export const Projects = () => {
+  const [categories, setCategories] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, checked } = e.target;
+    if (name === "allSelect") {
+      let tempCategorie = categories.map((categorie) => {
+        return { ...categorie, isChecked: checked };
+      });
+      setCategories(tempCategorie);
+    } else {
+      let tempCategorie = categories.map((categorie) =>
+        categorie.name === name
+          ? { ...categorie, isChecked: checked }
+          : categorie
+      );
+      setCategories(tempCategorie);
+    }
+  };
   useEffect(() => {
     document.title = "Projects";
+    let tempCategorie = filters.map((categorie) => {
+      return { ...categorie, isChecked: true };
+    });
+    setCategories(tempCategorie);
   }, []);
   return (
     <div className="Projects">
       <h1 className="title">MY WORK</h1>
-      <ProjectCard />
+      <div className="categorie">
+        <h2>Categories</h2>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              name="allSelect"
+              checked={
+                !categories.some((categorie) => categorie?.isChecked !== true)
+              }
+              onChange={handleChange}
+            />
+            All Select
+          </label>
+        </div>
+        {categories.map((categorie) => (
+          <div className="form-check">
+            <label className="form-check-label" key={categorie.id}>
+              <input
+                type="checkbox"
+                className="form-check-input"
+                name={categorie.name}
+                checked={categorie?.isChecked || false}
+                onChange={handleChange}
+              />
+              {categorie.name}
+            </label>
+          </div>
+        ))}
+      </div>
+      {projects.map((project) => {
+        if (
+          project.categorise.some(
+            (tag) =>
+              categories
+                .map((filter) => (filter.isChecked ? filter.name : null))
+                .indexOf(tag) > -1
+          )
+        ) {
+          return (
+            <div className="project">
+              <div className="hero-section">
+                <h3>{project.title}</h3>
+                <img
+                  src={process.env.PUBLIC_URL + project.img}
+                  alt={project.title}
+                />
+              </div>
+              <div className="info">
+                <div className="discription">{project.discription}</div>
+                <div className="link">
+                  {project.demoUrl ? (
+                    <a href={project.demoUrl}>
+                      <span className="demo">Demo</span>
+                    </a>
+                  ) : null}
+                  {project.sourceCodeUrl ? (
+                    <a href={project.sourceCodeUrl}>
+                      <BsGithub className="icon gh" />
+                    </a>
+                  ) : null}
+                </div>
+                {project.categorise.map((tag) => {
+                  return <div className="tag">{tag}</div>;
+                })}
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
